3f99a932ddd7a8cb0b5302048ad8b8cb40c35fe5
meheral1 <meherali1242006@gmail.com>
2026-03-07 00:10:15 +0500

    Solution 2

--- HackerRank-Prepare/PYTHON/Basic Datatype/Find the Runner-Up Score.py
+++ HackerRank-Prepare/PYTHON/Basic Datatype/Find the Runner-Up Score.py
@@ -1,2 +1,14 @@
 n = 5
-array = [2,3,6,6,5]
+arr = [2,3,6,6,5]
+
+Largest = float('-inf')
+second_Largest = float('-inf')
+
+for x in arr:
+    if x > Largest:
+        second_Largest = Largest
+        Largest = x
+    elif x > second_Largest and x < Largest:
+        second_Largest = x
+print(second_Largest)
+        
